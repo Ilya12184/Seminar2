2ddb2cd8f6a0892aff5ac2db605c352f8a8c8cf0
Ilya12184 <volostnov.il@yandex.ru>
2022-10-20 19:03:49 +0300

    Добавлена задача 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа. */
+
+int GetSecondNumber(int number)
+{
+    int SecondNumber = number / 10 % 10;
+    return SecondNumber;
+}
+
+int number = 785;
+int SecondNumber = GetSecondNumber(number);
+
+Console.WriteLine(SecondNumber);
