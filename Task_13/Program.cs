f3ecd418060220293517fc771a7d63e8b046e5d4
Ilya12184 <volostnov.il@yandex.ru>
2022-10-20 22:33:42 +0300

    Добавлена задача 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.*/
+
+int GetThreeNumber(int number)
+{
+    if (99<number & number<1000) 
+    {
+        number = number % 10;
+    }
+
+    if (number>1000)
+    {
+        while (number>1000)
+        {
+            number = number / 10;
+        }
+        number = number % 10;
+    }
+    else
+    {
+        Console.WriteLine("Третья цифра отсутствует");
+    } 
+
+    int ThreeNumber = number;
+    return ThreeNumber;
+}
+
+int number = 856239;
+int ThreeNumber = GetThreeNumber(number);
+
+Console.WriteLine(ThreeNumber);
+
